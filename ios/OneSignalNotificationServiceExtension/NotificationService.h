dfea9e271fbc519bf4580b7454dfa409b4f0124d
Alex S <alex@shkvoretz.com>
2022-12-20 06:27:53 +1100

    init commit

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Alex Shkvoretz on 20/12/2022.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
